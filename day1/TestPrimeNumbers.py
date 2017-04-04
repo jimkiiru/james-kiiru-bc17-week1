e9fdb6414d4967b38df11cf40e15a5d3bdadb79e
jimkiiru <kiiru.kurj@gmail.com>
2017-04-04 02:23:49 -0400

    Update TestPrimeNumbers.py

--- day1/TestPrimeNumbers.py
+++ day1/TestPrimeNumbers.py
@@ -6,6 +6,7 @@ from GeneratePrimeNumbers import GeneratePrimeNumbers
 class TestPrimeGenerator(unittest.TestCase):
 
     def TestCorrectness(self):
+        #test whether the function generates correct prime numbers
         self.assertEqual(GeneratePrimeNumbers(20), [2, 3, 5, 7, 11, 13, 17, 19])
 
 
@@ -16,9 +17,11 @@ class TestPrimeGenerator(unittest.TestCase):
         self.assertEqual(GeneratePrimeNumbers(1), "Zero or One cannot be prime numbers.")
 
     def TestString(self):
+        #makes sure that strings are not executed
         self.assertEqual(GeneratePrimeNumbers("String"), "Only integers are allowed.")
 
     def TestNegativeNumbers(self):
+        #makes sure that only positive numbers are processed if the number is negative an error is displayed
         self.assertEqual(GeneratePrimeNumbers(-1), "Not possible to generate prime numbers for integers less than 2.")
 
 
