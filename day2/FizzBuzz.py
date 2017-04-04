c36875ccb90a0e997dadcdb784c93e9ace82274a
jimkiiru <kiiru.kurj@gmail.com>
2017-04-04 15:34:37 -0400

    Create FizzBuzz.py

---
+++ day2/FizzBuzz.py
@@ -0,0 +1,9 @@
+def FizzBuzz(num):
+    if (num % 3 == 0):
+      print("Fizz")
+    elif (num % 5 ==0):
+      print("Buzz")
+    elif (num % 3 == 0 and num % 5 == 0):
+      print("FizzBuzz")
+    else:
+      print(num)
