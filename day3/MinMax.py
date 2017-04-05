a9f1dd3eb8b52c1340b87f253569b80a96b2b24c
jimkiiru <kiiru.kurj@gmail.com>
2017-04-04 21:55:58 -0400

    Update MinMax.py

--- day3/MinMax.py
+++ day3/MinMax.py
@@ -1,10 +1,11 @@
-
+''' This function outputs the minimum and the maximum number in a list '''
 def MinMax(ListNum):
    
-    if type(ListNum) is list:
-       if (min(ListNum)==max(ListNum)):
-           return [min(ListNum)]
-       else:
-           return [min(ListNum),max(ListNum)]  
-    else:
+   if type(ListNum) is list:
+      '''checks whether the minmum number and the maximum are equal'''
+      if (min(ListNum)==max(ListNum)):         
+         return [min(ListNum)]
+      else:
+         return [min(ListNum),max(ListNum)]  
+   else:
        return None
